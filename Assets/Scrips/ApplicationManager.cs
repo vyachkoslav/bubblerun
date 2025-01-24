a8d280ad02cb891207f9fe194629844302317ec9
Vyacheslav Denisov <vyach.denisov@gmail.com>
2025-01-24 21:58:52 +0200

    Fixed bubble pop sound not working when pressing start or quit

--- Assets/Scrips/ApplicationManager.cs
+++ Assets/Scrips/ApplicationManager.cs
@@ -1,15 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ApplicationManager : MonoBehaviour
 {
+    public float delay = 0.5f;
+    [SerializeField] private BubbleUIInteraction bubble;
+    
     public void SetScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
+    public void DelayedSetScene(string sceneName)
+    {
+        Destroy(bubble.gameObject);
+        StartCoroutine(RunDelayed(() => SetScene(sceneName)));
+    }
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void DelayedQuit()
+    {
+        Destroy(bubble.gameObject);
+        StartCoroutine(RunDelayed(Quit));
+    }
+
+    private IEnumerator RunDelayed(Action action)
+    {
+        yield return new WaitForSeconds(delay);
+        action();
     }
 }
--- Assets/Scrips/BubbleUIInteraction.cs
+++ Assets/Scrips/BubbleUIInteraction.cs
@@ -6,21 +6,29 @@ public class BubbleUIInteraction : MonoBehaviour
 {
     [SerializeField] private Transform bubbleSpawn;
     [SerializeField] private AudioSource bubbleSoundPlayer;
+    [SerializeField] private AudioSource bubbleDestroySoundPlayer;
+    
     private BubbleUI hoveredBubbleUI;
+    private bool interactionEnabled = true;
+    
+    public void SetEnabled(bool enabled)
+    {
+        interactionEnabled = enabled;
+    }
 
     private void OnMouseEnter()
     {
+        if (!interactionEnabled) return;
         bubbleSoundPlayer.Play();
         if(!hoveredBubbleUI)
             transform.position = bubbleSpawn.position;
         else
-        {
             hoveredBubbleUI.OnInteracted.Invoke();
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!interactionEnabled) return;
         if (other.TryGetComponent(out BubbleUI ui))
         {
             hoveredBubbleUI = ui;
@@ -30,10 +38,16 @@ public class BubbleUIInteraction : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (!interactionEnabled) return;
         if (hoveredBubbleUI && other.gameObject == hoveredBubbleUI.gameObject)
         {
             hoveredBubbleUI.OnHoverEnd.Invoke();
             hoveredBubbleUI = null;
         }
+    }
+
+    private void OnDestroy()
+    {
+        bubbleDestroySoundPlayer.Play();
     }
 }
