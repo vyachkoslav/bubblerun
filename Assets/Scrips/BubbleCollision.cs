976da11ed781658a4af0be5cc799f7c5afa7011a
Vyacheslav Denisov <vyach.denisov@gmail.com>
2025-01-26 01:59:19 +0200

    Added delay between collision sounds

--- Assets/Scrips/BubbleCollision.cs
+++ Assets/Scrips/BubbleCollision.cs
@@ -6,12 +6,21 @@ public class BubbleCollision : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip playerCollided;
     [SerializeField] private AudioClip wallCollided;
+    [SerializeField] private float delayBetweenSounds = 0.3f;
+    private double lastPlayerCollisionTime = double.MinValue;
+    private double lastWallCollisionTime = double.MinValue;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && lastPlayerCollisionTime + delayBetweenSounds < Time.time)
+        {
             audioSource.PlayOneShot(playerCollided);
-        else if(other.gameObject.CompareTag("Environment"))
+            lastPlayerCollisionTime = Time.time;
+        }
+        else if (other.gameObject.CompareTag("Environment") && lastWallCollisionTime + delayBetweenSounds < Time.time)
+        {
             audioSource.PlayOneShot(wallCollided);
+            lastWallCollisionTime = Time.time;
+        }
     }
 }
