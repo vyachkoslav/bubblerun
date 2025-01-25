350a9bc00ef8a2618208c406d23a2123e560c7aa
Vyacheslav Denisov <vyach.denisov@gmail.com>
2025-01-25 12:47:43 +0200

    After all bubbles die, returns to menu

---
+++ Assets/Scrips/GameStateManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private List<Transform> players = new();
+    [SerializeField] private UnityEvent OnBubblesLost = new();
+    [SerializeField] private UnityEvent OnBubblesWon = new();
+    private bool isEnded = false;
+
+    void Update()
+    {
+        if (isEnded) return;
+        
+        int alivePlayers = 0;
+        foreach (var player in players)
+        {
+            if (player != null && player.gameObject.activeSelf)
+                alivePlayers++;
+        }
+
+        if (alivePlayers == 0)
+        {
+            OnBubblesLost.Invoke();
+            isEnded = true;
+        }
+    }
+}
