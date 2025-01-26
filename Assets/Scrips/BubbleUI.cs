5e64547a3b22a4dfefb9a91e8f14aa83c3606840
Vyacheslav Denisov <vyach.denisov@gmail.com>
2025-01-26 10:48:55 +0200

    Added hover anim to menu buttons

--- Assets/Scrips/BubbleUI.cs
+++ Assets/Scrips/BubbleUI.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class BubbleUI : MonoBehaviour
 {
+    [SerializeField] private Color defaultColor = Color.white;
+    [SerializeField] private Color hoverColor = Color.white;
+    [SerializeField] private List<SpriteRenderer> renderers = new();
     private int hovered = 0;
 
     public void Interact()
@@ -13,16 +17,22 @@ public class BubbleUI : MonoBehaviour
     public void Hover()
     {
         hovered += 1;
-        if(hovered == 1)
+        if (hovered == 1)
+        {
+            renderers.ForEach(r => r.color = hoverColor);
             OnHover.Invoke();
+        }
     }
 
     public void UnHover()
     {
         Debug.Assert(hovered != 0);
         hovered -= 1;
-        if(hovered == 0)
+        if (hovered == 0)
+        {
+            renderers.ForEach(r => r.color = defaultColor);
             OnHoverEnd.Invoke();
+        }
     }
     [SerializeField] private UnityEvent OnInteracted = new();
     [SerializeField] private UnityEvent OnHover = new();
